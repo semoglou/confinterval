3a9e04ddffff0c375add6ebba4cbc1a5acf58257
Angelos Semoglou <a.semoglou@outlook.com>
2026-05-14 16:31:12 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="confinterval",
-    version="0.1.0",
+    version="0.1.1",
     description="Simple confidence intervals for Python.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
